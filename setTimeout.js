f55452d1cdbc4e83689168d94c249004d48b17cb
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-13 15:29:44 -0700

    Adding example of setTimeout method of window

---
+++ setTimeout.js
@@ -0,0 +1,8 @@
+// Calls a function or evaluates an expression after(despues) a specified number of milliseconds
+setTimeout(function(){
+	alert('Hi Criss');
+}, 3000);
+
+// 3000 milliseconds = 3 seconds
+// 30000 milliseconds = 30 seconds
+// 300000 milliseconds = 300 seconds = 5 min
